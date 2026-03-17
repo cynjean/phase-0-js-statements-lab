b5ccb0581699fdfc56eb74445c374410574402eb
cynjean <cynthia.2056@yahoo.com>
2026-03-17 16:00:07 -0500

    Statements Lab Completed

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,24 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// Watering Control
+if (temperature >= 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+// Garden Lights Control
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+// Soil Moisture Adjustment 
+while (soilMoisture <= 40) {
+    console.log(soilMoisture);
+    soilMoisture += 5;
+}
+    
